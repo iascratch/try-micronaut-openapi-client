15462a722cb87a76d257360725537be010e5a43c
Iwan Aucamp <aucampia@gmail.com>
2022-10-27 21:36:42 +0000

    checkpoint

--- kc/src/main/kotlin/com/example/DemoCommand.kt
+++ kc/src/main/kotlin/com/example/DemoCommand.kt
@@ -23,18 +23,6 @@ class DemoCommand : Runnable {
     val somePet = Pet("iadaingu", listOf())
     val mapper = jacksonObjectMapper()
     logger.info("somePet = {}", mapper.writeValueAsString(somePet))
-
-//    logger.info("getting sold pets ...")
-//    api.findPetsByStatus("sold").block().forEach {
-//      logger.info("pet.name = {}", it.name)
-//    }
-//    try {
-//      logger.info("adding a pet ...")
-//      val addedPet = api.addPet(Pet("iadaingu", listOf())).block()
-//      logger.info("added pet {}", addedPet)
-//    } catch (error: Exception) {
-//      logger.error("error = {}", error.message)
-//    }
     logger.info("done ...")
   }
 
